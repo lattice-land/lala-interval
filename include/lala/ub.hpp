19bb4f3865796b935e0bcfcf2ab9f6fb40f17cc6
ytsao <tasocc456@gmail.com>
2026-04-10 09:44:15 +0200

    Add one class for upper bounds

---
+++ include/lala/ub.hpp
@@ -0,0 +1,145 @@
+// Copyright 2026 Yi-Nung Tsao 
+
+#ifndef LALA_INTERVAL_UB_HPP
+#define LALA_INTERVAL_UB_HPP
+
+#include <type_traits>
+#include <utility>
+#include <cmath>
+#include <iostream>
+#include "battery/memory.hpp"
+
+namespace lala {
+
+template<class VT, class Mem = battery::local_memory>
+class UB
+{
+public:
+  using value_type = VT;
+  using memory_type = Mem;
+  using this_type = UB<value_type, memory_type>;
+  using basic_type = UB<value_type>;
+  using atomic_type = memory_type::template atomic_type<value_type>;
+
+  template<class VT2, class Mem2>
+  friend class UB;
+
+  constexpr static const bool is_totally_ordered = true;
+  constexpr static const char* name = "UB";
+
+private:
+  atomic_type value;
+
+  CUDA INLINE static constexpr this_type neg_inf() {
+    return battery::limits<value_type>::neg_inf();
+  }
+
+  CUDA INLINE static constexpr this_type inf() {
+    return battery::limits<value_type>::inf();
+  }
+
+public:
+  CUDA INLINE static constexpr this_type bot() { return neg_inf(); }
+  CUDA INLINE static constexpr this_type top() { return inf(); }
+
+  CUDA constexpr UB(): value(top()) {}
+  CUDA constexpr UB(value_type x): value(x) {}
+
+  template <class Mem2>
+  CUDA constexpr UB(const UB<value_type, Mem2>& other): value(other.load()) {}
+
+  constexpr UB(const this_type& other) = default;
+  constexpr UB(this_type&& other) = default;
+
+  CUDA INLINE constexpr this_type& operator=(value_type other) {
+    memory_type::store(value, other);
+    return *this;
+  }
+
+  CUDA INLINE constexpr value_type load() const {
+    return memory_type::load(value);
+  }
+
+  CUDA INLINE constexpr atomic_type& atomic() { return value; }
+  CUDA INLINE constexpr operator value_type() const { return load(); }
+
+  CUDA INLINE constexpr bool is_top() const {
+    return load() == top();
+  }
+
+  CUDA INLINE constexpr bool is_bot() const {
+    return load() == bot();
+  }
+
+  CUDA INLINE constexpr void join_top() {
+    memory_type::store(value, top());
+  }
+
+  CUDA INLINE constexpr bool join(basic_type other) {
+    if(load() < other.value) {
+      memory_type::store(value, other.value);
+      return true;
+    }
+    return false;
+  }
+
+  CUDA INLINE constexpr void meet_bot() {
+    memory_type::store(value, bot());
+  }
+
+  CUDA INLINE constexpr bool meet(basic_type other) {
+    if(load() > other.value) {
+      memory_type::store(value, other.value);
+      return true;
+    }
+    return false;
+  }
+
+  CUDA INLINE constexpr bool leq(basic_type other) const {
+    return load() <= other.value;
+  }
+
+  CUDA INLINE constexpr bool lt(basic_type other) const {
+    return load() < other.value;
+  }
+
+  CUDA NI void print() const {
+    if(is_bot()) {
+      printf("\u221E");
+    }
+    else if(is_top()) {
+      printf("-\u221E");
+    }
+    else {
+      ::battery::print(load());
+    }
+  }
+};
+
+template <class VT, class Mem>
+CUDA constexpr UB<VT, Mem> join(UB<VT, Mem> a, UB<VT, Mem> b) {
+  return battery::max(a.load(), b.load());
+}
+
+template <class VT, class Mem>
+CUDA constexpr UB<VT, Mem> meet(UB<VT, Mem> a, UB<VT, Mem> b) {
+  return battery::min(a.load(), b.load());
+}
+
+template <class VT, class Mem>
+std::ostream& operator<<(std::ostream &s, const UB<VT, Mem>& a) {
+  if(a.is_bot()) {
+    s << "\u221E";
+  }
+  else if(a.is_top()) {
+    s << "-\u221E";
+  }
+  else {
+    s << a.load();
+  }
+  return s;
+}
+
+} // namespace lala
+
+#endif
---
+++ tests/ub_test.cpp
@@ -0,0 +1,29 @@
+// Copyright 2026 Yi-Nung Tsao 
+
+#include <gtest/gtest.h>
+#include "lala/ub.hpp"
+#include "generic_lattice_test.hpp"
+
+using zub = lala::UB<int, battery::local_memory>;
+using azub = lala::UB<int, battery::atomic_memory<>>;
+using fub = lala::UB<float, battery::local_memory>;
+using afub = lala::UB<float, battery::atomic_memory<>>;
+
+template <class UB>
+void generic_UB_test() {
+  bot_top_test(UB(5));
+  EXPECT_TRUE(UB(10).lt(UB::top()));
+  EXPECT_TRUE(UB(0).lt(UB(10)));
+  EXPECT_TRUE(UB::bot().lt(UB(10)));
+  join_meet_generic_test(UB::bot(), UB::top());
+  join_meet_generic_test(UB(0), UB(0));
+  join_meet_generic_test(UB(0), UB(5));
+  join_meet_generic_test(UB(-10), UB(-5));
+}
+
+TEST(UBTest, LatticeOperation) {
+  generic_UB_test<zub>();
+  generic_UB_test<azub>();
+  generic_UB_test<fub>();
+  generic_UB_test<afub>();
+}
